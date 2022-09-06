1e878abd907d0347b44952e333dd87894ad7676d
Raven Barrogo <83223566+VEN-github@users.noreply.github.com>
2022-09-06 10:34:11 +0800

    Add rate & reviews (#5)
    * Add rate & reviews
    
    * resolve comments

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -23,6 +23,78 @@
         </div>
       </div>
     </div>
+    <div class="product__review">
+      <h3 class="product__review-title">Ratings & Reviews</h3>
+      <div class="product__review--flex">
+        <div class="product__review-empty" v-if="!reviews.length">
+          <img
+            class="product__review-empty-image"
+            src="../assets/empty-reviews.svg"
+            alt="Illustration for empty reviews"
+          />
+          <p class="product__review-empty-text">No reviews yet</p>
+        </div>
+        <ProductReview v-else :reviews="reviews" />
+        <div class="product__review-form">
+          <h4 class="product__review-form-title">Write a review</h4>
+          <form
+            @submit.prevent="submitReview"
+            class="product__review-form-group"
+          >
+            <div class="input-group">
+              <label class="label" for="name">Name</label>
+              <input
+                class="input product__review-input"
+                type="text"
+                name="name"
+                id="name"
+                v-model.trim="name"
+              />
+            </div>
+            <div class="input-group">
+              <label class="label" for="email">Email Address</label>
+              <input
+                class="input product__review-input"
+                type="email"
+                name="email"
+                id="email"
+                v-model.trim="email"
+              />
+            </div>
+            <div class="input-group">
+              <label class="label" for="rating">Rating</label>
+              <select
+                class="input product__review-input"
+                name="rating"
+                id="rating"
+                v-model.number="rating"
+              >
+                <option value="5">5 stars</option>
+                <option value="4">4 stars</option>
+                <option value="3">3 stars</option>
+                <option value="2">2 stars</option>
+                <option value="1">1 star</option>
+              </select>
+            </div>
+            <div class="input-group">
+              <label class="label" for="review">Review</label>
+              <textarea
+                class="input product__review-input"
+                name="review"
+                id="review"
+                rows="10"
+                v-model="review"
+              />
+            </div>
+            <input
+              type="submit"
+              class="btn btn_primary product__review-submit"
+              value="Submit a review"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
     <div class="product__related">
       <h3 class="product__related-title">Related Products</h3>
       <Splide :options="splideOptions">
@@ -38,6 +110,7 @@
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
 import '@splidejs/vue-splide/css';
 import ProductItem from '../components/ProductItem.vue';
+import ProductReview from '../components/ProductReview.vue';
 export default {
   name: 'ProductDetails',
   props: {
@@ -47,6 +120,7 @@ export default {
     Splide,
     SplideSlide,
     ProductItem,
+    ProductReview,
   },
   data() {
     return {
@@ -67,6 +141,11 @@ export default {
           },
         },
       },
+
+      name: null,
+      email: null,
+      rating: 5,
+      review: null,
     };
   },
   computed: {
@@ -80,6 +159,9 @@ export default {
     singleProduct() {
       return this.$store.getters.getSingleProduct ?? {};
     },
+    reviews() {
+      return this.$store.getters.getProductReviews ?? {};
+    },
     relatedProducts() {
       return this.$store.getters.getRelatedProducts ?? {};
     },
@@ -89,9 +171,27 @@ export default {
       this.$store.commit('setSingleProduct', '');
       this.$store.dispatch('loadSingleProduct', productId);
     },
+    submitReview() {
+      const { productId, email, rating, review } = this;
+      if (!this.name && !this.email && !this.review) {
+        alert('Please input empty fields');
+        return;
+      }
+      this.$store.dispatch('storeProductReviews', {
+        productId,
+        email,
+        rating,
+        review,
+      });
+      this.name = null;
+      this.email = null;
+      this.rating = 5;
+      this.review = null;
+    },
   },
   created() {
     this.loadSingleProduct(this.productId);
+    this.$store.dispatch('loadProductReviews');
   },
   watch: {
     productId(newId) {
